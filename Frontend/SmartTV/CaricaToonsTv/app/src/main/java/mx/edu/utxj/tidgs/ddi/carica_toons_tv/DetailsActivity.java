63347fa8c4709a3d56f984aeaf47e73ce87c2d20
ElisamaCalva <elisamaarturo.calvamoreno@utxicotepec.edu.mx>
2023-08-06 14:40:14 -0600

    Actualizado

---
+++ Frontend/SmartTV/CaricaToonsTv/app/src/main/java/mx/edu/utxj/tidgs/ddi/carica_toons_tv/DetailsActivity.java
@@ -0,0 +1,6 @@
+package mx.edu.utxj.tidgs.ddi.carica_toons_tv;
+
+import android.app.Activity;
+
+public class DetailsActivity extends Activity {
+}
---
+++ Frontend/SmartTV/CaricaToonsTv/app/src/main/java/mx/edu/utxj/tidgs/ddi/carica_toons_tv/MainActivity.kt
@@ -0,0 +1,15 @@
+package mx.edu.utxj.tidgs.ddi.carica_toons_tv
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+/**
+ * Loads [MainFragment].
+ */
+class MainActivity : FragmentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+    }
